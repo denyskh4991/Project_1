be961aeb36b1d858c9e4f2b2b6ede3b7baefdc95
denyskh4991 <denyskh4991@gmail.com>
2023-06-28 14:14:16 +0200

    Task #5 seventh

--- task#5.js
+++ task#5.js
@@ -37,3 +37,12 @@ for (let w=1; w<=500; w++) {
     middle = sum/w;
 }
 console.log(middle);
+
+let sum2 = 0;
+for (let s = 30; s <= 80; s++) {
+    if (s % 2 === 0){
+        sum2 += s;
+    }
+}
+
+console.log(sum2);
