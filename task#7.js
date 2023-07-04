967ff5618c42d39a1fec5816c6ec5e3a7c24374e
denyskh4991 <denyskh4991@gmail.com>
2023-07-04 14:54:30 +0200

    Task#7 completed

--- task#7.js
+++ task#7.js
@@ -1,33 +1,36 @@
 function askForNumbers() {
+
     let a = null;
     let b = null;
     let c = null;
 
     while (a === null || isNaN(a)) {
-        a = parseFloat(prompt("Enter a"));
-        if (a === null) {
-            console.log("I'm sorry to hear that. Hope to see you again");
+        const inputA = prompt("Будемо знаходити рішення квадратного рівняння виду: ax^2 + bx + c = 0\nВведіть a");
+        if (inputA === null) {
+            alert('Шкода. Сподіваюсь ще побачитись');
             return;
         }
+        a = parseFloat(inputA);
     }
 
     while (b === null || isNaN(b)) {
-        b = parseFloat(prompt("Enter b"));
-        if (b === null) {
-            console.log("I'm sorry to hear that. Hope to see you again");
+        const inputB = prompt("a="+a+"\nВведіть b");
+        if (inputB === null) {
+            alert('Шкода. Сподіваюсь ще побачитись');
             return;
         }
+        b = parseFloat(inputB);
     }
 
     while (c === null || isNaN(c)) {
-        c = parseFloat(prompt("Enter c"));
-        if (c === null) {
-            console.log("I'm sorry to hear that. Hope to see you again");
+        const inputC = prompt("a="+a+", b="+b+"\nВведіть c");
+        if (inputC === null) {
+            alert('Шкода. Сподіваюсь ще побачитись');
             return;
         }
+        c = parseFloat(inputC);
     }
 
-    console.log(a, b, c);
     quadraticEquation(a, b, c);
 }
 
@@ -35,19 +38,23 @@ askForNumbers();
 
 function quadraticEquation (a, b, c) {
     let D = b * b - 4 * a * c;
+    if (a==0){
+        alert("Рівняння не має рішень");
+        return;
+    }
     if (D<0){
-        console.log("The equation has no solutions");
+        alert("Рівняння не має рішень");
         return;
     }
     if (D==0) {
         let x1 = -b / (2 * a);
-        console.log("The only root of the equation ",x1);
+        alert("Єдиний корень рівняння: "+x1+"");
         return;
     }
     if (D>0) {
         let x2 = (-b + Math.sqrt(D))/(2 * a);
         let x3 = (-b - Math.sqrt(D))/(2 * a);
-        console.log("The roots of the equation: ",x2, x3);
+        alert("Корені рівняння: "+x2+", "+x3+"");
         return;
     }
 }
