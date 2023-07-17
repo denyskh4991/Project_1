b9d5c9e96981a8fbd0376e07a45a5ed69fb116cf
denyskh4991 <denyskh4991@gmail.com>
2023-07-17 12:46:18 +0200

    Task#11 completed

--- task#11.js
+++ task#11.js
@@ -1,8 +1,11 @@
-const array = [1, 2, 3, 4, 5, 6, 7];
-
 function removeElement(array, elemValue) {
+    const indexToRemove = array.indexOf(elemValue);
 
+    if (indexToRemove !== -1) {
+        array.splice(indexToRemove, 1);
+    }
 }
 
-removeElement(array, 5 );
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 4);
 console.log(array);
