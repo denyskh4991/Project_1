2c6efc127ea2f8c634fc93ae65ccc101a114cf67
denyskh4991 <denyskh4991@gmail.com>
2023-08-02 12:04:59 +0200

    Task#18 formal changes

--- task#18.js
+++ task#18.js
@@ -1,11 +1,36 @@
-function findDates() {
-    let rgRes = strMain.matchAll(/(?<year>\d{4})?-(?<month>\d{2})-(?<day>\d{2})/g);
-    let item = 0;
-    for (let item of rgRes){
-        console.log(item);
+function findDates(inputString) {
+    const dateRegex = /\b(\d{4})-(\d{2})-(\d{2})\b/g;
+    const monthNames = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+
+    const dates = [];
+    let match;
+    while ((match = dateRegex.exec(inputString)) !== null) {
+        const year = parseInt(match[1]);
+        const month = parseInt(match[2]);
+        const day = parseInt(match[3]);
+
+        if (month < 1 || month > 12) {
+            continue;
+        }
+
+        const maxDayOfMonth = new Date(year, month, 0).getDate();
+        if (day < 1 || day > maxDayOfMonth) {
+            continue;
+        }
+
+        dates.push({
+            day,
+            month,
+            monthName: monthNames[month - 1],
+            year
+        });
     }
-    return item;
+
+    return dates;
 }
 
-const strMain = "The dates between 1976-03-12, and 1983-10-14, the code was 2004-67-12 and possible invalid date 8765-11-34";
-findDates(strMain);
+const str = "The dates between 1976-03-12, and 1983-10-14, the code was 2004-67-12 and possible invalid date 8765-11-34";
+console.log(findDates(str));
