f1aa4b3316836f82e2b77262d1645631a2d1e909
denyskh4991 <denyskh4991@gmail.com>
2023-08-03 16:52:57 +0200

    Task #17 finished

--- task#17.js
+++ task#17.js
@@ -1,8 +1,14 @@
-const phoneNumbers = ['380 44 364 83 65', '+380 44 364 83 65', '380-44-364-83-65', '+380-44-364-83-65'];
+const phoneNumbers = ['380 44 364 83 65', '+380 44 364 83 65', '380-44-364-83-65', '+380-44-364-83-65', '+380443648365'];
 
 const results = phoneNumbers.map(str => {
     const phoneRegex = /^(\+380|380)( |-)\d{2}( |-)\d{3}( |-)\d{2}( |-)\d{2}$/;
-    return phoneRegex.test(str) ? str : null;
+    if (phoneRegex.test(str)) {
+        const numberParts = str.match(/\d+/g);
+        const number = numberParts.join('').replace(/^(\+?380|380)/, '0');
+        return number;
+    } else {
+        return null;
+    }
 });
 
 console.log(results);
