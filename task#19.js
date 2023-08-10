44aeb4dc7f4c66b7fa29e55b0293d0890f70b430
denyskh4991 <denyskh4991@gmail.com>
2023-08-10 14:00:34 +0200

    Task#19 completed

--- task#19.js
+++ task#19.js
@@ -1,7 +1,4 @@
 class Man {
-    name = ['John', 'Michael', 'Bobb'];
-    age = [17, 18, 20];
-
     constructor(name, age) {
         this.name = name;
         this.age = age;
@@ -9,11 +6,6 @@ class Man {
 }
 
 class Car {
-    brand = ['Toyota', 'Nissan', 'Mercedes'];
-    model = ['Supra', 'Skyline', 'Vito'];
-    year = [1986, 2002, 2001];
-    plate = ['АА1234ВА', 'АА0000ВА', 'АІ0434ВН'];
-
     constructor(brand, model, year, plate) {
         this.brand = brand;
         this.model = model;
@@ -24,35 +16,43 @@ class Car {
     assignOwner(owner) {
         this.owner = owner;
         if (this.owner.age > 18) {
-            console.log("Owner Info:");
-            this.owner.assignOwner();
+            console.log(`Owner ${this.owner.name} is old enough.`);
+            this.displayOwnerInfo();
         } else {
-            console.log("Owner is not old enough.");
+            console.log(`Owner ${this.owner.name} is not old enough.`);
         }
+    }
+
+    displayOwnerInfo() {
+        console.log(`Owner: ${this.owner.name}, Age: ${this.owner.age}`);
     }
 
     displayCarAndOwnerInfo() {
         console.log(`Car: ${this.brand}, ${this.model}, ${this.year}, ${this.plate}`);
         if (this.owner) {
-            console.log(`Name: ${this.owner.name}, Age: ${this.owner.age}`);
+            console.log(`Owner: ${this.owner.name}, Age: ${this.owner.age}`);
         } else {
             console.log("No owner assigned yet.");
         }
     }
 }
 
-const man = new Man(['John', 'Michael', 'Bobb'], [17, 18, 20]);
-console.log('Man:', man);
+const man1 = new Man('John', 17);
+const man2 = new Man('Michael', 18);
+const man3 = new Man('Bobb', 20);
 
-const car = new Car(
-    ['Toyota', 'Nissan', 'Mercedes'],
-    ['Supra', 'Skyline', 'Vito'],
-    [1986, 2002, 2001],
-    ['АА1234ВА', 'АА0000ВА', 'АІ0434ВН']
-);
-console.log('Car:', car);
+const car1 = new Car('Toyota', 'Supra', 1986, 'АА1234ВА');
+const car2 = new Car('Nissan', 'Skyline', 2002, 'АА0000ВА');
+const car3 = new Car('Mercedes', 'Vito', 2001, 'АІ0434ВН');
 
-car.assignOwner(man);
+console.log('Car 1:', car1);
+console.log('Car 2:', car2);
+console.log('Car 3:', car3);
 
-car.displayCarAndOwnerInfo();
+car1.assignOwner(man1);
+car2.assignOwner(man2);
+car3.assignOwner(man3);
+car1.displayCarAndOwnerInfo();
+car2.displayCarAndOwnerInfo();
+car3.displayCarAndOwnerInfo();
 
