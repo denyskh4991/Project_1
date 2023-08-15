09fdf682c91922d5a7ef59fa130745bb74546521
denyskh4991 <denyskh4991@gmail.com>
2023-08-15 17:05:49 +0200

    Task#21 fixed

--- task#21.js
+++ task#21.js
@@ -22,23 +22,23 @@ class Hamburger {
     }
 }
 
-const SIZE_SMALL = { price: 50, calories: 20 };
-const SIZE_LARGE = { price: 100, calories: 40 };
+Hamburger.SIZE_SMALL = { price: 50, calories: 20 };
+Hamburger.SIZE_LARGE = { price: 100, calories: 40 };
 
-const STUFFING_CHEESE = { price: 10, calories: 20 };
-const STUFFING_SALAD = { price: 20, calories: 5 };
-const STUFFING_POTATO = { price: 15, calories: 10 };
+Hamburger.STUFFING_CHEESE = { price: 10, calories: 20 };
+Hamburger.STUFFING_SALAD = { price: 20, calories: 5 };
+Hamburger.STUFFING_POTATO = { price: 15, calories: 10 };
 
-const TOPPING_SPICE = { price: 15, calories: 0 };
-const TOPPING_MAYO = { price: 20, calories: 5 };
+Hamburger.TOPPING_SPICE = { price: 15, calories: 0 };
+Hamburger.TOPPING_MAYO = { price: 20, calories: 5 };
 
-var hamburger = new Hamburger(SIZE_SMALL, STUFFING_CHEESE);
+var hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
 
-hamburger.addTopping(TOPPING_MAYO);
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
 
 console.log("Calories: " + hamburger.calculateCalories());
 console.log("Price: " + hamburger.calculatePrice());
 
-hamburger.addTopping(TOPPING_SPICE);
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
 
 console.log("Price with spice: " + hamburger.calculatePrice());
