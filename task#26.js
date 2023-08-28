7199515e38a942effdeb6f68ff6efeb9f4e7f483
denyskh4991 <denyskh4991@gmail.com>
2023-08-28 14:13:43 +0200

    Task#26 fixed

--- task#26.js
+++ task#26.js
@@ -1,24 +1,15 @@
-var form = document.getElementById("tableForm");
 var table = document.getElementById("table");
 
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
+var num = 1;
+for (var i = 0; i < 10; i++) {
+    var row = document.createElement("tr");
 
-    table.innerHTML = "";
-
-    var num = 1;
-    for (var i = 0; i < 10; i++) {
-        var row = document.createElement("tr");
-
-        for (var j = 0; j < 10; j++) {
-            var cell = document.createElement("td");
-            cell.textContent = num;
-            row.appendChild(cell);
-            num++;
-        }
-
-        table.appendChild(row);
+    for (var j = 0; j < 10; j++) {
+        var cell = document.createElement("td");
+        cell.textContent = num;
+        row.appendChild(cell);
+        num++;
     }
-});
 
-form.dispatchEvent(new Event('submit'));
+    table.appendChild(row);
+}
