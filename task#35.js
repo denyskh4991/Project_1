414b5dfe7ee67bc0b43a68162c98495c9bd7442f
denyskh4991 <denyskh4991@gmail.com>
2023-09-26 21:38:38 +0200

    Task#35 fixed

--- task#35.js
+++ task#35.js
@@ -1,20 +1,22 @@
-function getWeather() {
-    var xhr = new XMLHttpRequest();
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19";
+async function getWeather() {
+    const url = "http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19";
 
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var data = JSON.parse(xhr.responseText);
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
             document.getElementById("temp").textContent = data.main.temp;
             document.getElementById("pressure").textContent = data.main.pressure;
             document.getElementById("description").textContent = data.weather[0].description;
             document.getElementById("humidity").textContent = data.main.humidity;
             document.getElementById("speed").textContent = data.wind.speed;
             document.getElementById("deg").textContent = data.wind.deg;
-            document.getElementById("icon").src = "http://openweathermap.org/img/w/10d.png";        }
-    };
-    xhr.send();
+        } else {
+            console.error("Помилка при виконанні запиту");
+        }
+    } catch (error) {
+        console.error("Помилка:", error);
+    }
 }
 
 getWeather();
