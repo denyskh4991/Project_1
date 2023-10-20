a7e0ad0506c10f7a17933292d7307ab01f0a6935
denyskh4991 <denyskh4991@gmail.com>
2023-10-20 17:01:16 +0200

    Task#39 fixed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-/*import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';*/
+import "./App.css";
 
 // Компонент хедера
 function Header() {
@@ -9,43 +9,6 @@ function Header() {
       </header>
   );
 }
-
-// Компонент лівої бічної навігації
-/*function Sidebar() {
-  return (
-      <aside>
-        <nav>
-          <ul>
-            <li><Link to="/">Головна</Link></li>
-            <li><Link to="/about">Про нас</Link></li>
-            <li><Link to="/contact">Контакти</Link></li>
-          </ul>
-        </nav>
-      </aside>
-  );
-}*/
-
-// Компонент центрального контейнера
-/*function MainContent() {
-  return (
-      <main>
-        <Switch>
-          <Route path="/about">
-            <h2>Про нас</h2>
-            <p>Опис вашого додатку</p>
-          </Route>
-          <Route path="/contact">
-            <h2>Контакти</h2>
-            <p>Контактна інформація</p>
-          </Route>
-          <Route path="/">
-            <h2>Головна</h2>
-            <p>Зміст головної сторінки</p>
-          </Route>
-        </Switch>
-      </main>
-  );
-}*/
 
 function App() {
   return (
@@ -61,8 +24,6 @@ function App() {
               <p>
                   I live in Warsaw
               </p>
-            {/*<Sidebar />*/}
-            {/*<MainContent />*/}
           </div>
         </div>
 
